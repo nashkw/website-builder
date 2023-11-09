62a4c4c93bfc0a5ff71b82c5ea410564d83c6aa1
Nash Waugh <nwaugh444@gmail.com>
2023-11-09 16:25:27 +0000

    added alt text to icons
    added alt text to icon svg elements

--- src/ftb-website-builder/resources/js/Components/Icons/AccountIcon.vue
+++ src/ftb-website-builder/resources/js/Components/Icons/AccountIcon.vue
@@ -3,9 +3,23 @@
 </script>
 
 <template>
-    <svg class="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+    <svg
+        class="w-3.5 h-3.5"
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+        aria-labelledby="accountIconAltText"
+    >
+        <title id="accountIconAltText">
+            Account Icon
+        </title>
         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+        <path
+            fill-rule="evenodd"
+            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+        />
     </svg>
 </template>
 
--- src/ftb-website-builder/resources/js/Components/Icons/MenuIcon.vue
+++ src/ftb-website-builder/resources/js/Components/Icons/MenuIcon.vue
@@ -9,7 +9,11 @@
         height="16"
         fill="currentColor"
         viewBox="0 0 16 16"
+        aria-labelledby="menuIconAltText"
     >
+        <title id="menuIconAltText">
+            Menu icon
+        </title>
         <path
             fill-rule="evenodd"
             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
