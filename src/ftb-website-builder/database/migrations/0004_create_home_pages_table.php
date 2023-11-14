39045c090e99908de841b8f71bb19174dc271a80
Nash Waugh <nwaugh444@gmail.com>
2023-11-14 17:50:52 +0000

    migrations for all page tables
    migrations to create tables for each set of pages
    
    note that these field types are not necessarily final

---
+++ src/ftb-website-builder/database/migrations/0004_create_home_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('home_pages', function (Blueprint $table) {
+            $table->foreignId('property_id');
+            $table->string('meta_page_title')->nullable();
+            $table->string('meta_page_description')->nullable();
+            $table->string('cover_image_primary');
+            $table->string('cover_image_primary_description')->nullable();
+            $table->string('intro_section_header');
+            $table->string('intro_section_paragraph');
+            $table->string('intro_section_image')->nullable();
+            $table->string('intro_section_image_description')->nullable();
+            $table->string('welcome_section_header');
+            $table->string('welcome_section_paragraph');
+            $table->string('welcome_section_image')->nullable();
+            $table->string('welcome_section_image_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('home_pages');
+    }
+};
---
+++ src/ftb-website-builder/database/migrations/0006_create_rooms_pages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms_pages', function (Blueprint $table) {
+            $table->foreignId('property_id');
+            $table->string('meta_page_title')->nullable();
+            $table->string('meta_page_description')->nullable();
+            $table->string('rooms_page_section_header');
+            $table->string('rooms_page_section_paragraph')->nullable();
+            $table->string('rooms_page_section_image')->nullable();
+            $table->string('rooms_page_section_image_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms_pages');
+    }
+};
