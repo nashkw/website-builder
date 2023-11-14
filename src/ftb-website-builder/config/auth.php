8a584c8dc566eaf1a3962fe08618a3e8480210e5
Nash Waugh <nwaugh444@gmail.com>
2023-11-14 15:07:03 +0000

    migrations cleanup
    changed migration names to custom naming convention
    
    removed obsolete migrations

--- src/ftb-website-builder/config/auth.php
+++ src/ftb-website-builder/config/auth.php
@@ -93,7 +93,6 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
         ],
