1e93293a678a2958bf2e40ace1510ed21f71e88b
Nash Waugh <nwaugh444@gmail.com>
2024-02-02 12:44:36 +0000

    account test updates
    removed blank example tests
    
    moved password update tests into account test as the account management page is the only place where this functionality is accessed
    
    added a test for updating the subdomain

--- src/ftb-website-builder/tests/Feature/AccountTest.php
+++ src/ftb-website-builder/tests/Feature/AccountTest.php
@@ -6,6 +6,7 @@ use App\Models\Property;
 use App\Models\User;
 use App\Models\Website;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AccountTest extends TestCase
@@ -46,6 +47,66 @@ class AccountTest extends TestCase
 
         $this->assertSame('Test User', $user->name);
         $this->assertSame('test@example.com', $user->email);
+    }
+
+    public function test_subdomain_can_be_updated(): void
+    {
+        $user = User::factory()
+            ->has(Property::factory()->has(Website::factory()))
+            ->create();
+        $newSubdomain = fake()->domainWord;
+
+        $response = $this
+            ->actingAs($user)
+            ->post('/account', [
+                'subdomain' => $newSubdomain,
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/account');
+
+        $user->refresh();
+
+        $this->assertSame($newSubdomain, $user->property->website->subdomain);
+    }
+
+    public function test_password_can_be_updated(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->from('/account')
+            ->put('/password', [
+                'current_password' => 'password',
+                'password' => 'new-password',
+                'password_confirmation' => 'new-password',
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/account');
+
+        $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
+    }
+
+    public function test_correct_password_must_be_provided_to_update_password(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->from('/account')
+            ->put('/password', [
+                'current_password' => 'wrong-password',
+                'password' => 'new-password',
+                'password_confirmation' => 'new-password',
+            ]);
+
+        $response
+            ->assertSessionHasErrors('current_password')
+            ->assertRedirect('/account');
     }
 
     public function test_user_can_delete_their_account(): void
